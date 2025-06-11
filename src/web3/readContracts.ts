9b7cbbd8f2728d16a9c069ac0060dd9cc5af92bf
RasAlghur <bakaretoyosi2017@gmail.com>
2025-06-11 09:57:50 -0700

    new

--- src/web3/readContracts.ts
+++ src/web3/readContracts.ts
@@ -3,16 +3,16 @@ import { LAUNCHER_ABI, SAFU_LAUNCHER_CA } from "./config";
 
 export const pureMetrics = await publicClient.readContract(
   {
-    ...LAUNCHER_ABI as any,
+    ...LAUNCHER_ABI,
     address: SAFU_LAUNCHER_CA as `0x${string}`,
-    functionName: 'getMetrics'
+    functionName: 'getMetrics',
   }
 );
 
 export const pureInfoDataRaw = async (tokenAddress: any) => {
   if (!tokenAddress) return;
   return await publicClient.readContract({
-    ...LAUNCHER_ABI as any,
+    ...LAUNCHER_ABI,
     address: SAFU_LAUNCHER_CA,
     functionName: 'data',
     args: [tokenAddress],
