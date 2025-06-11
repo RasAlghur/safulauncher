cbbb610520ff9c36a8d983eba4439bca4ad3a995
RasAlghur <bakaretoyosi2017@gmail.com>
2025-06-11 10:09:35 -0700

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
+// https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: "es2022", // Or 'esnext'
+    assetsInlineLimit: 4096, // Increase asset caching
+  },
   plugins: [react()],
   server: {
     proxy: {
@@ -10,6 +16,12 @@ export default defineConfig({
         target: 'http://localhost:4000',
         changeOrigin: true,
       },
+    },
+    hmr: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
